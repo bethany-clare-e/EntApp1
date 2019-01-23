995a2b119086a412a03ef3318604b87f6cb333f3
Bethany Endersby <S00190486@mail.itsligo.ie>
2019-01-23 16:57:57 +0000

    Final Commit with Razor Pages

---
+++ EntApp1/Pages/greeting.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EntApp1.Pages
+{
+    public class greetingModel : PageModel
+    {
+
+        public string Message { get; set; } = "Hello";
+
+
+        public void OnGet()
+        {
+            if (DateTime.Now.Hour >= 12)
+            {
+                Message = "Good Afternoon";
+            }
+
+            else
+            {
+                Message = "Good Morning";
+            }
+          
+
+        }
+    }
+}
