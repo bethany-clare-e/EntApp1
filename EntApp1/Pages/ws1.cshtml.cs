21ef23845e195e88aad423c6164769142c681055
Bethany Endersby <S00190486@mail.itsligo.ie>
2019-01-29 22:03:10 +0000

    commit ws1 and ws1b

---
+++ EntApp1/Pages/ws1.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EntApp1.Pages
+{
+    public class ws1Model : PageModel
+    {
+        public string Message { get; set; }
+        public string[] MyTestArray { get; set; }
+            = { "England", "Ireland", "Scotland", "Wales" };
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost(string firstname)
+        {
+            Message = 
+                string.Format("Hello {0}", firstname);
+        }
+    }
+}
---
+++ EntApp1/Pages/ws1b.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EntApp1.Pages
+{
+    public class ws1bModel : PageModel
+    {
+        public string Message { get; set; }
+        public string Calculation { get; set; }
+        
+        public void OnPost(int number1, int number2)
+        {
+            int result = (number1) + (number2);
+            Message =
+                string.Format("The sum of {0} and {1} is {2} ", number1, number2, result);
+            
+            
+        }
+
+        
+    }
+}
